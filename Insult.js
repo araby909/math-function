8f31435aaf5a225f8639baaa0a94fb9cca28cb10
araby909 <30875419+araby909@users.noreply.github.com>
2017-08-21 09:09:13 -1000

    Add files via upload

---
+++ Insult.js
@@ -0,0 +1,5 @@
+  function InsultsRandom() {
+    var Insults = ["Boooiii", "poopoo head", "IM SCHLEEP"];
+    var randomNumber = Math.floor(Math.random()*3);
+    window.alert(Insults[randomNumber]);
+  }
